32ee3b8b31f8688bd88fdac19267b97d5367081d
Gabriel <65578956+gab98fra@users.noreply.github.com>
2023-02-07 16:38:15 -0600

    Create palindrome.scala

---
+++ palindrome.scala
@@ -0,0 +1,25 @@
+/*
+    Scala version: 2.11.12
+*/
+
+//This is function remove gap
+def removeGap(param: String):String={param.replace(" ", "")}
+
+//Example 1
+val phrasesOne=List("This is a phrases ", "String ")
+val remove_gap_one=phrasesOne.map(removeGap)                                                   
+if (remove_gap_one(0)==remove_gap_one(1).reverse){println("This is a palindrome")}else{println("This is not a palindrome")}
+//Example 2
+val phrasesTwo=List("malayalam", "malayalam ")
+val remove_gap_two=phrasesTwo.map(removeGap) 
+if (remove_gap_two(0)==remove_gap_two(1).reverse){println("This is a palindrome")}else{println("This is not a palindrome")}
+//Example  3
+val phrasesThree=List("level")
+val phrasesThree1="level"::phrasesThree//add an item
+val remove_gap_three=phrasesThree1.map(removeGap) 
+if (remove_gap_three(0)==remove_gap_three(1).reverse){println("This is a palindrome")}else{println("This is not a palindrome")}
+
+//Example  4
+val phrasesFour="level " :: "level "::Nil
+val remove_gap_four=phrasesFour.map(removeGap) 
+if (remove_gap_four(0)==remove_gap_four(1).reverse){println("This is a palindrome")}else{println("This is not a palindrome")}
