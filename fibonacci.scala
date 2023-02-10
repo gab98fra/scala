ef4f32690891529293cd25d91496c793422af3d0
Gabriel <65578956+gab98fra@users.noreply.github.com>
2023-02-10 17:09:59 -0600

    Update fibonacci.scala

--- fibonacci.scala
+++ fibonacci.scala
@@ -3,7 +3,7 @@
 */
 
 def fibonacci(n: Int)={
-   	var n1=0
+    var n1=0
     var n2=1
     var aux=0
     var count=0
@@ -11,7 +11,7 @@ def fibonacci(n: Int)={
    if (n==1){1}
    if (n>1){
       while(count < n){
-				aux=n1 + n2
+	aux=n1 + n2
         println(n1)
         count +=1
         n1=n2
